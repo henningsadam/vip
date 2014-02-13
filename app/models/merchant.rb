ce677ae0904229839104f6708a3e3048a7ba1a6f
Adam Hennings <hennings.adam@gmail.com>
2014-02-12 22:10:43 -0700

    implements simple_form on add merchant, deletes scaffold styles, adds validations in merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,9 @@
 class Merchant < ActiveRecord::Base
   attr_accessible :company_name, :dba_name, :merchant_id, :institution_id
 
-  validates :merchant_id, :uniqueness => true
+  validates :merchant_id, 	:uniqueness => true,
+  							:presence => true
+  validates :institution_id, :presence => true
 
   belongs_to :institution
 end
