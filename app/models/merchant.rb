f1630f84020010fa03828427e32d316f16fd2187
Adam Hennings <hennings.adam@gmail.com>
2014-02-13 18:08:02 -0700

    add validation on models, simple form upgrades

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,9 @@
 class Merchant < ActiveRecord::Base
   attr_accessible :company_name, :dba_name, :merchant_id, :institution_id
 
-  validates :merchant_id, 	:uniqueness => true,
-  							:presence => true
-  validates :institution_id, :presence => true
+  validates :merchant_id, 	:uniqueness => true
+  validates :institution_id,:merchant_id, :company_name, :dba_name,
+  					:presence => true
 
   belongs_to :institution
 end
